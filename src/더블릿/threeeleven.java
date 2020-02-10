0f0faaaa0a0ae4267bf6d6c3401985ca5c648ac9
anseohyun <60808168+anseohyun@users.noreply.github.com>
2020-02-10 16:21:10 +0900

    1

--- src/더블릿/threeeleven.java
+++ src/더블릿/threeeleven.java
@@ -1,10 +1,39 @@
 package ´õºí¸´;
 
+import java.util.Scanner;
+
 public class threeeleven {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	Scanner sc = new Scanner(System.in);
+	
+	int arr []  = new int[7];
+	for(int i = 0; i<7;i++)
+	{
+		arr[i] = sc.nextInt();
 	}
-
+	
+	
+	int max = arr[0];
+	for(int p = 0; p<6; p++)
+	{
+		if(max < arr[p+1])
+		{
+			max = arr[p+1];
+		}
+	}
+	
+	int min = arr[0];	
+	for(int j = 0; j<6;j++)
+	{
+		if(min > arr[j+1])
+		{
+			min = arr[j+1];
+		}
+	}
+	
+	System.out.println(max + " "+ min);
+	
+}
 }
