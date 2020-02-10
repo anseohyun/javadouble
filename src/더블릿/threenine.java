e919f3d3f4ada8f4ec19ab4dec0768abd559f037
anseohyun <60808168+anseohyun@users.noreply.github.com>
2020-02-10 16:45:22 +0900

    1

---
+++ src/더블릿/threenine.java
@@ -0,0 +1,48 @@
+//	프로그램 명: max
+//	제한시간: 1 초
+//	7 개의 수를 입력으로 받아 입력 받은 수 중 최대값을 출력하는 프로그램을 작성하시오.
+//
+//	입력
+//	7 개의 수는 100 이하의 자연수이다.
+//	출력
+//	최대값
+//
+//	입출력 예
+//	입력
+//	6 2 9 8 3 4 7
+//	출력
+//	9
+
+package 더블릿;
+
+import java.util.Scanner;
+
+public class threenine {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int arr[] = new int[7];
+		
+		for(int i=0 ; i < 7 ; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = arr[0];
+		
+		for(int j = 1 ; j < 7 ; j++)
+		{
+			if(max<arr[j])
+			{
+				max = arr[j];
+			}
+		}
+		
+		System.out.println(max);
+		
+
+	}
+
+}
